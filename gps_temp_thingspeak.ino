e2acb6a26e6ae102b2c4178cf31bbe675ed36e52
rajaathota72-fc <57040106+rajaathota72-fc@users.noreply.github.com>
2020-04-10 18:27:37 +0530

    Add files via upload

---
+++ gps_temp_thingspeak.ino
@@ -0,0 +1,180 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+#include <DHT.h> 
+#define DHTPIN 5
+/*
+   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 12, TXPin = 13;
+static const uint32_t GPSBaud = 9600;
+
+// repace your wifi username and password
+const char* ssid     = "raju";
+const char* password = "123456789";
+const char* server = "api.thingspeak.com";
+
+unsigned long myChannelNumber = 818134;
+const char * myWriteAPIKey = "PZELI1WWWU2BUPDK";
+String apiKey = "PZELI1WWWU2BUPDK";
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+DHT dht(DHTPIN, DHT11);
+WiFiClient  client;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  ss.begin(GPSBaud);
+  dht.begin();
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println();
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+   Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  ThingSpeak.begin(client);
+  
+}
+
+void loop()
+{
+ 
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+ 
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field3=";
+                             postStr += String(t);
+                             postStr +="&field4=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%. Send to Thingspeak.");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  
+  // thingspeak needs minimum 15 sec delay between updates, i've set it to 30 seconds
+  delay(10000);
+  
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+ // Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+
+    double latitude = (gps.location.lat());
+    double longitude = (gps.location.lng());
+    
+    String latbuf;
+    latbuf += (String(latitude, 6));
+    Serial.println(latbuf);
+
+    String lonbuf;
+    lonbuf += (String(longitude, 6));
+    Serial.println(lonbuf);
+
+    ThingSpeak.setField(1, latbuf);
+    ThingSpeak.setField(2, lonbuf);
+    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);  
+    delay(20000);
+    
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+}
